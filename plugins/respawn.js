5ee5c243ef720253deff4e4b9fd54f7615662c4f
Adam Brady <adam@volatiletechnology.com>
2012-10-31 18:03:24 +1100

    woops

---
+++ plugins/respawn.js
@@ -0,0 +1,15 @@
+module.exports = function(game) {
+  game.on("player:join", function(player) {
+    player.client.on("packet:03", function(packet) {
+      var tmp = packet.message.split(" ");
+      if(tmp[0]=="/suicide") {
+        player.kill();
+      }
+    });
+    player.client.on("packet:cd", function(packet) {
+      if(packet.payload) {
+        player.respawn();
+      }
+    });
+  });
+};
