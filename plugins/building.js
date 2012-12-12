590d436c4a83239c482fdf7ffeca611c946e1537
SomeoneWeird <adam@volatiletechnology.com>
2012-12-12 22:57:13 +1100

    partially fix building
    .

--- plugins/building.js
+++ plugins/building.js
@@ -4,19 +4,50 @@ module.exports = function() {
 
       player.client.on("packet:0f", function(packet) {
 
-          var chunk_x = packet.x >> 4,
-              chunk_z = packet.z >> 4;
+          var tmp_x = packet.x,
+              tmp_z = packet.z,
+              tmp_y = packet.y;
 
-          var block_x = packet.x & 0x0f,
-              block_z = packet.z & 0x0f,
-              block_y = packet.y;
+          switch(packet.direction) {
+            case 0: {
+              tmp_y -= 1;
+              break;
+            }
+            case 1: {
+              tmp_y += 1;
+              break;
+            }
+            case 2: {
+              tmp_z -= 1;
+              break;
+            }
+            case 3: {
+              tmp_z += 1;
+              break;
+            }
+            case 4: {
+              tmp_x -= 1;
+              break;
+            }
+            case 5: {
+              tmp_x += 1;
+              break;
+            }
+          }
+
+          var chunk_x = tmp_x >> 4,
+              chunk_z = tmp_z >> 4;
+
+          var block_x = tmp_x & 0x0f,
+              block_z = tmp_z & 0x0f,
+              block_y = tmp_y;
 
           game.map.get_abs_chunk(packet.x, packet.z, function(err, chunk) {
 
-            chunk.set_block_type(block_x, block_z, block_y, packet.slot.data);
+            chunk.set_block_type(block_x, block_z, block_y, packet.slot.block);
 
             game.clients.forEach(function(client) {
-              client.emit("data", {pid: 0x35, x: block_x, y: block_y, z: block_z, type: packet.slot.data, metadata: 0});
+              client.emit("data", {pid: 0x35, x: tmp_x, y: tmp_y, z: tmp_z, type: packet.slot.block, metadata: 0});
             });
           });
       });
